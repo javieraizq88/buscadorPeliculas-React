b78611b432f0dc8b779a7ec6f40f4dd857fe73ad
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-01 18:04:16 -0400

    dando estilos al home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,56 @@
 import React, { useState } from "react";
 import { Container, Card, Grid, Typography, TextField, Button } from "@material-ui/core"
+import styles from "./style";
 
 export default () => {
     const [searchText, setSearchText] = useState("");
+    const clases = styles();
+
+    
 
     const handleSearchTextChange = e => {
         setSearchText(e.target.value);
     };
 
-   const hanleCleanTextClick = e => {
-    console.log(10);
+    const hanleCleanTextClick = e => {
+        console.log("hice click en limpiar");
 
-   };
-   const handleSearchTextClick = e => {
-    console.log(12);
-   };
+    };
+    const handleSearchTextClick = e => {
+        console.log("hice click en buscar");
+    };
     console.log(searchText);
 
     return (
-        <Container>
-            <Card>
-                <Grid container>
+        <Container className={clases.container}>
+            <Card className={clases.cardContainer}>
+                <Grid container className={clases.titleGridContainer}>
                     <Grid>
-                        <Typography>
+                        <Typography className={clases.title}>
                             Bienvenido!
                         </Typography>
                     </Grid>
                     <Grid>
-                        <lavel>
-                            Icono
-                        </lavel>
+                        <lavel>Icono</lavel>
                     </Grid>
                 </Grid>
                 <TextField
                     value={searchText}
                     placeholder="Buscar..."
                     onChange={handleSearchTextChange}
+                    className={clases.textFieldSearch}
                 />
-                <Grid>
+                <Grid className={clases.buttonsContainer}>
                     <Button variant="contained" onClick={hanleCleanTextClick}>Limpiar</Button>
-                    <Button variant="contained" color="primary" size="large" onClick={handleSearchTextClick}>Buscar</Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        onClick={handleSearchTextClick}
+                        className={clases.searchButton}
+                    >
+                        Buscar
+                        </Button>
 
                 </Grid>
             </Card>
