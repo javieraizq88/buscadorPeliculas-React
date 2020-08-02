c91b0ae53f8bf97f24742908e6dc98eca7479cc8
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-02 19:14:13 -0400

    sin key

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://www.omdbapi.com/?apiKey=ffd0c3a5"; 
+// const baseURL = "https://www.omdbapi.com/?apiKey=ffd0c3a5"; 
 
 export const apiCall = (url, data, headers, method) =>  axios ({
     method,
