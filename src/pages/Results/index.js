58067578cfb586e6d191ece7f9612361587487e8
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-02 20:51:30 -0400

    cargando resultados de peliculas de IMDB

--- src/pages/Results/index.js
+++ src/pages/Results/index.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Container } from "@material-ui/core";
+import { Container, CircularProgress } from "@material-ui/core";
 import queryString from "query-string"; //hace parse del query que viene en la url y lo transforma en un object
 import { searchMovie } from "../../redux/actions/search";
 import { movieResults, isSearchLoading } from "../../redux/selector";
+import MovieResult from "../../components/MovieResult";
 
 export default ({ location }) => {
     const dispatch = useDispatch();
     const movies = useSelector(state => movieResults(state));
+    const isLoading = useSelector(state => isSearchLoading(state));
     console.log(movies);
 
     useEffect(() => {
@@ -19,9 +21,21 @@ export default ({ location }) => {
 
     });
 
+    const renderMovies = () => {
+        if (movies) {
+            return movies.map((value, index) => <MovieResult key={index} {...value} />);
+        } else if (isLoading) {
+            return <CircularProgress size={100} color="primary" />
+        }
+        // si movies existe, devuelve componentes de MovieResult
+        // movies.map para recorrer el arreglo 
+        // {...value} le paso las propiedades del objeto
+        return <div></div>;
+    };
+
     return (
         <Container>
-            Results
+            {renderMovies()}
         </Container>
     )
 }
