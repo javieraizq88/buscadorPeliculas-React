5c8c561740dd6a2e3e2e9433f2fc8820c1f74ee2
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-02 21:03:28 -0400

    estilos de resultados

--- src/components/MovieResult/index.js
+++ src/components/MovieResult/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Card, Grid, Typography, Button } from "@material-ui/core";
 
+import style from "./style";
+
 const MovieResult = ({ Title, Year, Type, imdID, Poster }) => {
+    const clases = style();
+
     return (
-        <Card >
+        <Card className={clases.cardContainer}>
             <Grid container>
                 <Grid item>
-                    <img src={Poster} alt={Title} />
+                    <img
+                        src={Poster}
+                        alt={Title}
+                        className={clases.poster} />
                 </Grid>
-                <Grid item>
+                <Grid item className={clases.titlesContainer} > 
                     <Typography> {Title} </Typography>
                     <Typography> {Year} </Typography>
                     <Typography> {Type} </Typography>
---
+++ src/components/MovieResult/style.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+
+export default makeStyles({
+    cardContainer: {
+        marginBottom: 8
+    },
+    poster: {
+        width: 120
+    },
+    titlesContainer: {
+        paddingLeft: 8
+    }
+
+})
