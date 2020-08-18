b1865980aadd1540be1d419822e7eb201ca65159
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-18 17:07:37 -0400

    actualizacion

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // hay q borrar la API key cada vez q lo suba al git
-const baseURL = "https://www.omdbapi.com/?apiKey=ffd0c3a5";
+const baseURL = "";
 
 export const apiCall = (url, data, headers, method) =>
     axios({
